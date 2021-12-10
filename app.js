e37450f3a957571bff72f8f3d4f2337ceddba65c
jaak69 <jk.ivask@gmail.com>
2021-12-10 10:24:59 +0200

    vana nägu enam vähem taastatud. Menüü ja jalus vaja CSS HTML korrigeerida

--- app.js
+++ app.js
@@ -23,9 +23,117 @@ app.get('/posts', (req, res) => {
     /* res.sendFile('./views/index.html', { root: __dirname }); */
 
     let posts = [
-        {id : 1, title : "Esimene postitus", sisu: "Igavene jama" },
-        {id : 2, title : "Teine postitus", sisu: "Lõplik jama" },
-        {id : 3, title : "Teine postitus", sisu: "PUPU" },
+        {
+            "userId": 1,
+            "id": 1,
+            "user_data": {
+              "name": "John Doe",
+              "email": "john.doe@example.com"
+            },
+            "caption": "I think it's going to rain",
+            "date": "2021-10-21T05:55:06.419Z",
+            "post_image": "https://drive.google.com/uc?export=view&id=18Rk7E056Y3J90d-PfTEFRIDfHWtXsMCv"
+          },
+          {
+            "userId": 2,
+            "id": 2,
+            "user_data": {
+              "name": "Mick Jagger",
+              "email": "mick99@example.com"
+            },
+            "caption": "Which weighs more, a pound of feathers or a pound of bricks?",
+            "date": "2021-10-21T06:06:06.419Z",
+            "post_image": ""
+          },
+          {
+            "userId": 1,
+            "id": 3,
+            "user_data": {
+              "name": "John Doe",
+              "email": "john.doe@example.com"
+            },
+            "caption": "Felt cute, might delete later",
+            "date": "2021-10-21T07:12:06.419Z",
+            "post_image": "https://drive.google.com/uc?export=view&id=18Rk7E056Y3J90d-PfTEFRIDfHWtXsMCv"
+          },
+          {
+            "userId": 2,
+            "id": 4,
+            "user_data": {
+              "name": "Mick Jagger",
+              "email": "mick99@example.com"
+            },
+            "caption": "Something, something, something?",
+            "date": "2021-10-21T08:06:06.419Z",
+            "post_image": ""
+          },
+          {
+            "userId": 1,
+            "id": 5,
+            "user_data": {
+              "name": "John Doe",
+              "email": "john.doe@example.com"
+            },
+            "caption": "Felt, felt, felt",
+            "date": "2021-10-21T09:12:06.419Z",
+            "post_image": "https://drive.google.com/uc?export=view&id=18Rk7E056Y3J90d-PfTEFRIDfHWtXsMCv"
+          },
+          {
+            "userId": 1,
+            "id": 6,
+            "user_data": {
+              "name": "John Doe",
+              "email": "john.doe@example.com"
+            },
+            "caption": "I think it's going to rain",
+            "date": "2021-10-21T10:55:06.419Z",
+            "post_image": "https://drive.google.com/uc?export=view&id=18Rk7E056Y3J90d-PfTEFRIDfHWtXsMCv"
+          },
+          {
+            "userId": 2,
+            "id": 7,
+            "user_data": {
+              "name": "Mick Jagger",
+              "email": "mick99@example.com"
+            },
+            "caption": "Which weighs more, a pound of feathers or a pound of bricks?",
+            "date": "2021-10-21T11:06:06.419Z",
+            "post_image": ""
+          },
+          {
+            "userId": 1,
+            "id": 8,
+            "user_data": {
+              "name": "John Doe",
+              "email": "john.doe@example.com"
+            },
+            "caption": "Felt cute, might delete later",
+            "date": "2021-10-21T12:12:06.419Z",
+            "post_content": "<p>Barbara selina toymaker america deadshot bullock pennyworth calendar grayson. Luthor league thorne chimera grayson manor the gargoyle luthor doom! Swamp lazarus manor justice joker luthor zeus lazarus aquaman al quinn? Quinn czonk phantom joe batcave ghul zeus wayne. Ra wayne ivy toymaker. Martha zatanna batgirl bartok czonk ragman luthor shade. Clayface chimera night clayface nocturna raatko vale. Face killer master catwoman joker. Anarky.</p><p>Shade penguin maroni kane tumbler prey green ghul owl todd red gotham tally. America nocturna phantom abdullah. Joe ragman face, czonk cain tim atkins arrow ragman. Grundy rhino catwoman faith todd hatter canary ghul. Metallo phantom shrike al scorn carmine aquaman zucco oswald outsider canary batman. Atkins oracle crane barbara fox arkham joker raatko edward rupert kyle katana. Caird moth mad alfred alcor ivy ragdoll abbott.</p>",
+            "post_image": "https://drive.google.com/uc?export=view&id=18Rk7E056Y3J90d-PfTEFRIDfHWtXsMCv"
+          },
+          {
+            "userId": 2,
+            "id": 9,
+            "user_data": {
+              "name": "Mick Jagger",
+              "email": "mick99@example.com"
+            },
+            "caption": "Something, something, something?",
+            "date": "2021-10-21T13:06:06.419Z",
+            "post_image": ""
+          },
+          {
+            "userId": 1,
+            "id": 10,
+            "user_data": {
+              "name": "John Doe",
+              "email": "john.doe@example.com"
+            },
+            "caption": "Felt, felt, felt",
+            "date": "2021-10-21T14:12:06.419Z",
+            "post_image": "https://drive.google.com/uc?export=view&id=18Rk7E056Y3J90d-PfTEFRIDfHWtXsMCv"
+          }
     ]
     res.render('posts', {posts});
 });
